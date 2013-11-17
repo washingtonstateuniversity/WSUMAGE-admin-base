59271a1f45540080d9b8b68a77b5bfd3bcaf4861
jeremyBass <jeremybass@cableone.net>
2013-11-17 12:42:19 -0800

    file cleanup and removes jQuery to utilities

--- app/code/community/Wsu/AdminTheme/Model/Adminhtml/Observer.php
+++ app/code/community/Wsu/AdminTheme/Model/Adminhtml/Observer.php
@@ -7,18 +7,7 @@ class Wsu_AdminTheme_Model_Adminhtml_Observer {
             Mage::getDesign()->setTheme($type, $theme);
         }
     }
-	//note that we are doing the folder clearing due to the admin html
-	//cache not being cleared when you flush the caches.  Need to look into this later
 	public function cleanConfigCache(){
-		// Init without cache so we get a fresh version
-		$cache=Mage::getBaseDir('cache');
-		
-		$files = glob($cache.'/*/*'); // get all file names
-		foreach($files as $file){ // iterate files
-		  if(is_file($file)){
-			unlink($file); // delete file
-		  }
-		}
-		Mage::getSingleton('core/session')->addSuccess('Saved config cache.');
+		Mage::helper('storeutilities')->cleanConfigCache();
 	}
 }
