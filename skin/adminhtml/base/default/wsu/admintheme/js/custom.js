60bc8582dcc69914514a379d18b72c79bb3ad629
jeremyBass <jeremybass@cableone.net>
2013-11-18 11:40:03 -0800

    corrections in js and default settings

--- skin/adminhtml/base/default/wsu/admintheme/js/custom.js
+++ skin/adminhtml/base/default/wsu/admintheme/js/custom.js
@@ -2,7 +2,6 @@
 
 (function($){
 	$(function(){
-		
 		function setup_fixedNav(){
 			if ($(window).scrollTop()>= 122) { $('body').addClass('fixed');  }
 			$(window).scroll(function (event) {
@@ -14,19 +13,20 @@
 			});
 		}
 		setup_fixedNav();
-		
-		$("#system_config_tabs dt").filter(function() {
-			 return !$('*', this).hasClass('active');
-		}).addClass("close");
-		$("#system_config_tabs dt").filter(function() {
-			 return $('*', $(this).siblings()).hasClass('active');
-		}).addClass("open");
-		$("#system_config_tabs dd").filter(function() {
-			 return !$('*', $(this).siblings()).hasClass('active');
-		}).hide();
-		$("#system_config_tabs dt").on("click",function(){
-			$(this).siblings().toggle();
-			$(this).toggleClass("open","close");
-		});
+		if($("#system_config_tabs").length){
+			$("#system_config_tabs dt").filter(function() {
+				 return !$('*', this).hasClass('active');
+			}).addClass("close");
+			$("#system_config_tabs dt").filter(function() {
+				 return $('*', $(this).siblings()).hasClass('active');
+			}).addClass("open");
+			$("#system_config_tabs dd").filter(function() {
+				 return !$('*', $(this).siblings()).hasClass('active');
+			}).hide();
+			$("#system_config_tabs dt").on("click",function(){
+				$(this).siblings().toggle();
+				$(this).toggleClass("open","close");
+			});
+		}
 	});
-}(jQuery));
+})(jQuery);
